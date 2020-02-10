7af844f0c2649422fcf44f9e5546bd49ca13cb29
14911Ace <58379366+14911Ace@users.noreply.github.com>
2020-02-10 21:36:02 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi, I'm not reactapp</h1>
+     <Person/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const person = () =>
+ {return <p>This is it, we are getting somethings out of here,.</p>}
+
+ export default person;
